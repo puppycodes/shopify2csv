6a76a345c7ac48fa395783811a713984f964c7ab
puppycodes <pupcodes@gmail.com>
2019-03-26 18:30:56 -0400

    update with dot env file

--- export.js
+++ export.js
@@ -6,12 +6,13 @@ const _ = require('underscore');
 const RateLimiter = require('limiter').RateLimiter;
 const flatten = require('flat');
 const limiter = new RateLimiter(1, 500);
+const credentials = require('dotenv').config();
 
-let apikey = '';
-let password = '';
-let shopname = '';
+let apikey = process.env.API_KEY;
+let password = process.env.PASSWORD;
+let storeName = process.env.STORE_NAME;
 
-let baseurl = 'https://'+apikey+':'+password+'@'+shopname+'.myshopify.com';
+let baseurl = 'https://'+apikey+':'+password+'@'+storeName+'.myshopify.com';
 let numOrders = 0;
 let ordersList = [];
 
@@ -47,8 +48,8 @@ request({
 	if (!error && response.statusCode === 200) {
 		numOrders = body.count;
 	}
-
-	console.log('Total Order Count :'+numOrders);
+	console.log();
+	console.log('Processing: '+numOrders);
 	console.log();
 	let numPages = numOrders / 250;
 	let r = _.range(1, numPages+1);
@@ -60,15 +61,16 @@ request({
 
 	}, function(err) {
 		// Called when all are finished
-		console.log('Total orders: '+ordersList.length)
+		console.log('Total: '+ordersList.length);
+		console.log();
 
 		json2csv( {data: ordersList, fields : ['closed_at','created_at','email',
 		'total_price','browser_ip', 'shipping_address.city', 'shipping_address.province']},
 			function(err, csv) {
 				if (err) console.log(err);
-				  fs.writeFile('ShopifyOrders.csv', csv, function(err) {
+				  fs.writeFile('orders.csv', csv, function(err) {
 				    if (err) throw err;
-				    console.log('File saved');
+				    console.log('Done! saved as "orders.csv"');
 				  });
 			});
 	});
